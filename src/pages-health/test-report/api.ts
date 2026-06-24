3604c5bb545797d2537dc6e34654aef8923bb276
zhigang.zheng <zhigang.zheng@zhaopin.com.cn>
2026-06-24 22:11:48 +0800

    feat: 删除无关代码

--- src/pages-health/test-report/api.ts
+++
@@ -1,8 +0,0 @@
-import type { IResponse } from '@/http/types'
-import { http } from '@/http/alova'
-
-export function getEmotionTestDetail(id: number | string) {
-  return http.Post<IResponse<Record<string, any>>>('/api/emotionTest/getDetail', {
-    id: String(id),
-  })
-}
